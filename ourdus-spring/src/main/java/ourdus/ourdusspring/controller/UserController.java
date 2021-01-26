7e5cc30535987915331147a9452fa2175d3d13bb
DESKTOP-26K4CPA\SeoyeonKim <2018103585@khu.ac.kr>
2021-01-26 23:32:53 +0900

    test error

--- ourdus-spring/src/main/java/ourdus/ourdusspring/controller/UserController.java
+++ ourdus-spring/src/main/java/ourdus/ourdusspring/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import ourdus.ourdusspring.dto.UserDTO;
 import ourdus.ourdusspring.service.UserService;
 
-@Controller
+@RestController
 public class UserController {
 
     private final UserService userService;
@@ -18,28 +18,22 @@ public class UserController {
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping("/user/login")
-    @ResponseBody
     public String login(@RequestBody UserDTO loginUser){
         return userService.login(loginUser);
     }
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping("/user/join")
-    @ResponseBody
     public String join(@RequestBody UserDTO newUser){
         return userService.join(newUser);
     }
 
 
     @DeleteMapping("/user/{id}")
-    @ResponseBody
     public String delete(@PathVariable("id") Long id){
         return userService.delete(id);
     }
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping("/user/{id}")
-    @ResponseBody
     public String update(@RequestBody UserDTO user){return userService.update(user);}
 
 
--- ourdus-spring/src/test/java/ourdus/ourdusspring/service/UserServiceTest.java
+++ ourdus-spring/src/test/java/ourdus/ourdusspring/service/UserServiceTest.java
@@ -3,10 +3,13 @@ package ourdus.ourdusspring.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import ourdus.ourdusspring.SpringConfig;
 import ourdus.ourdusspring.domain.User;
 import ourdus.ourdusspring.dto.UserDTO;
 //import ourdus.ourdusspring.repository.SpringDataJpaUserRepository;
@@ -18,17 +21,19 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+//@EnableConfigurationProperties(value= SpringConfig.class)
 class UserServiceTest {
 
-    @Autowired
+//    @Autowired
     private UserService userService;
 
     @Autowired
     private UserRepository userRepository;
 
     @BeforeEach
-    public void setup(){
-        userService = new UserService(userRepository);
+    public void beforeEach(){
+        userService= new UserService(userRepository);
     }
 
     @Test
@@ -37,7 +42,7 @@ class UserServiceTest {
        UserDTO user1 =new UserDTO();
        user1.setEmail("abc@naver.com");
        UserDTO user2 = new UserDTO();
-       user2.setEmail("abc@navercom");
+       user2.setEmail("abc@naver.com");
         //when
         userService.join(user1);
         //then
